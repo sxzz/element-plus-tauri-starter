b964225de3175127a790da5d3e6e2c5ab8038667
三咲智子 <sxzz@sxzz.moe>
2022-06-16 23:04:43 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { app } from '@tauri-apps/api'
+const i = ref(0)
+
+let name = $ref('')
+let version = $ref('')
+let tauriVersion = $ref('')
+
+;(async () => {
+  name = await app.getName()
+  version = await app.getVersion()
+  tauriVersion = await app.getTauriVersion()
+})()
+</script>
+
+<template>
+  <h1 style="color: var(--el-color-primary)">
+    <i-ep-element-plus /> Hello Element Plus!
+  </h1>
+
+  <el-button @click="i++">Incrase: {{ i }}</el-button>
+
+  <el-descriptions title="App">
+    <el-descriptions-item label="Name">
+      {{ name }}
+    </el-descriptions-item>
+    <el-descriptions-item label="Version">
+      {{ version }}
+    </el-descriptions-item>
+    <el-descriptions-item label="Tauri Version">
+      {{ tauriVersion }}
+    </el-descriptions-item>
+  </el-descriptions>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  padding: 0 30px;
+}
+</style>
